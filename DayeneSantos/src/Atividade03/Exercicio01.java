53685969132f9d26992af5685d777bb3d7846d66
DayeneS <dayene-santos@hotmail.com>
2025-04-07 13:07:53 -0300

    Solicita um número inteiro e exibe o valor digitado

---
+++ DayeneSantos/src/Atividade03/Exercicio01.java
@@ -0,0 +1,21 @@
+package Atividade03;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.println("Digite um número: ");
+			num = sc.nextInt();
+			
+		System.out.println("O número digitado foi: " + num);
+		
+			
+		sc.close();
+	}
+
+}
