b0f405d4cf4fe9d2f6281fcb6e26bfdb41794fff
DayeneS <dayene-santos@hotmail.com>
2025-04-07 17:49:26 -0300

    Exibe mensagem centralizada dentro de uma caixa de caracteres

--- DayeneSantos/src/Atividade04/Exercicio11.java
+++ DayeneSantos/src/Atividade04/Exercicio11.java
@@ -1,5 +1,41 @@
+/* Exercicio11 application
+ * 
+ * @ author Dayene dos Santos Rosa - 12127152
+ * @ version 1.00 2025/03/26
+ */
+
 package Atividade04;
 
+import java.util.Scanner;
+
 public class Exercicio11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char letra1, letra2, letra3, letra4, letra5;
+		
+		System.out.print("Digite a primeira letra: ");
+			letra1 = sc.nextLine().charAt(0);
+		
+		System.out.print("Digite a segunda letra: ");
+			letra2 = sc.nextLine().charAt(0);
+			
+		System.out.print("Digite a terceira letra: ");
+			letra3 = sc.nextLine().charAt(0);
+		
+		System.out.print("Digite a quarta letra: ");
+			letra4 = sc.nextLine().charAt(0);
+		
+		System.out.print("Digite a quinta letra: ");
+			letra5 = sc.nextLine().charAt(0);	
+			
+		System.out.printf("Veja as letras digitadas lado a lado: %c, %c, %c, %c, %c.\n", letra1, letra2, letra3, letra4, letra5);
+		System.out.printf("Veja as letras digitadas uma abaixo da outra: \n %c \n %c \n %c \n %c \n %c \n", letra1, letra2, letra3, letra4, letra5);	
+		
+		sc.close();
+
+	}
 
 }
---
+++ DayeneSantos/src/Atividade04/Exercicio12.java
@@ -0,0 +1,31 @@
+/* Exercicio12 application
+ * 
+ * @ author Dayene dos Santos Rosa - 12127152
+ * @ version 1.00 2025/03/26
+ */
+
+package Atividade04;
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(" ___________________________________________ ");
+		System.out.println("|                                           |");
+		System.out.println("|                                           |");
+		System.out.println("|                                           |");
+		System.out.println("|          #SOMOSTODOSNEWTONWYDEN           |");
+		System.out.println("|          ----------------------           |");
+		System.out.println("|                                           |");
+		System.out.println("|                                           |");
+		System.out.println("|___________________________________________|");
+		
+		sc.close();
+
+	}
+
+}
