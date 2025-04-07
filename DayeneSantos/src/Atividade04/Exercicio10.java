b84e9b96918dc47be77553489661667e83e6ce1e
DayeneS <dayene-santos@hotmail.com>
2025-04-07 17:45:18 -0300

    Lê cinco letras e exibe na mesma ordem em linhas separadas e na mesma linha

--- DayeneSantos/src/Atividade04/Exercicio10.java
+++ DayeneSantos/src/Atividade04/Exercicio10.java
@@ -1,5 +1,30 @@
+/* Exercicio10 application
+ * 
+ * @ author Dayene dos Santos Rosa - 12127152
+ * @ version 1.00 2025/03/25
+ */
 package Atividade04;
 
+import java.util.Scanner;
+
 public class Exercicio10 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int idade;
+		double altura;
+		
+		System.out.print("Digite uma idade: ");
+			idade = sc.nextInt();
+			
+		System.out.print("Digite uma altura: ");
+			altura = sc.nextDouble();
+			
+		System.out.printf("A idade digitada foi: %d anos, e a altura digitada foi %.2f de altura\n", idade, altura);
+		
+		sc.close();
+	}
 
 }
