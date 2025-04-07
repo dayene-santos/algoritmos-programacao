7811f36cfa209dddd464cf2be4bb7769593319a8
DayeneS <dayene-santos@hotmail.com>
2025-04-07 17:54:43 -0300

    Lê letras 'a' e 'r' e imprime 'arara' com uma única chamada ao printf

--- DayeneSantos/src/Atividade04/Exercicio14.java
+++ DayeneSantos/src/Atividade04/Exercicio14.java
@@ -1,5 +1,29 @@
+/* Exercicio14 application
+ * 
+ * @ author Dayene dos Santos Rosa - 12127152
+ * @ version 1.00 2025/03/26
+ */
 package Atividade04;
 
+import java.util.Scanner;
+
 public class Exercicio14 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char letra1, letra2;
+		
+		letra1 = 'A';
+		letra2 = 'R';
+	
+		
+		System.out.printf("A palavra é: %c%c%c%c%c",letra1, letra2, letra1, letra2, letra1);
+		
+		
+		sc.close();
+
+	}
 
 }
