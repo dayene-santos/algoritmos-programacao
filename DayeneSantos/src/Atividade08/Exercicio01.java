16bc200990e4769846b382755098f44167fa4654
DayeneS <dayene-santos@hotmail.com>
2025-05-27 11:14:46 -0300

    Commit Inicial, e Exercicio01

---
+++ DayeneSantos/src/Atividade08/Exercicio01.java
@@ -0,0 +1,19 @@
+/* Exercicio01 application
+ *
+ * @ author Dayene dos Santos Rosa - 12127152
+ * @ version 1.00 2025/05/26
+ */
+package Atividade08;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        sc.close(); // fechar o scanner (boas práticas)
+    }
+}
